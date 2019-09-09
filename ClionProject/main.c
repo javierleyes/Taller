08991085e40202056fdac76a78110ff2b0cda233
Javi <javier.nunezleyes@gmail.com>
2019-09-09 19:24:58 -0300

    comandos client

--- ClionProject/main.c
+++ ClionProject/main.c
@@ -50,23 +50,80 @@ int main (int argc, char *argv[])
 {
 //    return validar_modo_operacion(argc, argv);
 
-    printf("Sudoku \n");
+//  comando client
 
-    tablero_t *tablero = tablero_inicializar("board.txt");
+    char *input = calloc(13, sizeof(char));
+    char *comando = calloc(7, sizeof(char));
+    char *comando_compuesto = calloc(3, sizeof(char));
+    char *fila = calloc(1, sizeof(char));
+    char *columna = calloc(1, sizeof(char));
 
-    tablero_get(tablero);
+    fgets(input, 13, stdin);
+    strncpy(comando, input, sizeof(comando));
 
-    tablero_verify(tablero);
+    if (strcmp(comando, "verify\n") == 0) {
+        printf("V");
+    }
 
-    tablero_put(tablero, 7, 5, 8);
+    if (strcmp(comando, "get\n") == 0) {
+        printf("G");
+    }
 
-    tablero_verify(tablero);
+    if (strcmp(comando, "reset\n") == 0) {
+        printf("R");
+    }
 
-    tablero_resetear(tablero);
+    if (strcmp(comando, "exit\n") == 0) {
+        printf("E");
+    }
 
-    tablero_get(tablero);
+    strncpy(comando_compuesto, input, 3);
 
-    tablero_exit(tablero);
+    if (strcmp(comando_compuesto, "put") == 0) {
+
+        if ((((input[9]-'0') > 0) && ((input[9]-'0') < 10) ) && (((input[11]-'0') > 0) && ((input[11]-'0') < 10))) {
+            printf("Indices validos");
+        } else {
+            printf("Error en los indices. Rango soportado: [1,9]\n");
+        }
+
+        if ( ((input[4] - '0') > 0) && ((input[4] - '0') < 10) ) {
+            printf("valor valido");
+        } else {
+            printf("Error en el valor ingresado. Rango soportado: [1,9]\n");
+        }
+    }
+
+    free(input);
+    free(comando);
+    free(comando_compuesto);
+    free(fila);
+    free(columna);
+
+    printf("Modo no soportado, el primer parametro debe ser server o client\n");
+
+
+
+
+
+
+//    printf("Sudoku \n");
+//
+//    tablero_t *tablero = tablero_inicializar("board.txt");
+//
+//    tablero_get(tablero);
+//
+//    tablero_verify(tablero);
+//
+//    tablero_put(tablero, 7, 5, 8);
+//
+//    tablero_verify(tablero);
+//
+//    tablero_resetear(tablero);
+//
+//    tablero_get(tablero);
+//
+//    tablero_exit(tablero);
 
     return 0;
 }
