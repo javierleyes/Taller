23a5a85e8047043bfb1ed4f49e4827a040a7033c
Javi <javier.nunezleyes@gmail.com>
2019-09-14 20:24:24 -0300

    refactor comandos clientes

--- ClionProject/Cliente.c
+++ ClionProject/Cliente.c
@@ -8,57 +8,44 @@ void cliente_inicializar() {
     char *comando = calloc(8, sizeof(char));
     char *comando_compuesto = calloc(4, sizeof(char));
 
-    do {
-        fgets(input, 13, stdin);
+    fgets(input, 13, stdin);
+
+    while(strcmp(input, "exit\n") != 0) {
 
         strncpy(comando, input, (7 * sizeof(char)));
 
-        if (strcmp(comando, "exit\n") == 0) {
-            printf("E\n");
-            break;
-        }
-
         if (strcmp(comando, "verify\n") == 0) {
             printf("V\n");
-            continue;
-        }
-
-        if (strcmp(comando, "get\n") == 0) {
+        } else if (strcmp(comando, "get\n") == 0) {
             printf("G\n");
-            continue;
-        }
-
-        if (strcmp(comando, "reset\n") == 0) {
+        } else if (strcmp(comando, "reset\n") == 0) {
             printf("R\n");
-            continue;
-        }
+        } else {
+            strncpy(comando_compuesto, input, 3);
 
-//        strncpy(comando_compuesto, input, 3 * sizeof(char));
-        strncpy(comando_compuesto, input, 3 );
+            if (strcmp(comando_compuesto, "put") == 0) {
 
-        if (strcmp(comando_compuesto, "put") == 0) {
-
-            if ((((input[9] - '0') > 0) && ((input[9] - '0') < 10)) && (((input[11] - '0') > 0) && ((input[11] - '0') < 10))) {
+                if ((((input[9] - '0') > 0) && ((input[9] - '0') < 10)) &&
+                    (((input[11] - '0') > 0) && ((input[11] - '0') < 10))) {
 //            printf("Indices validos");
 
-                if (((input[4] - '0') > 0) && ((input[4] - '0') < 10)) {
-                    printf("P\n");
-                    continue;
+                    if (((input[4] - '0') > 0) && ((input[4] - '0') < 10)) {
+                        printf("P\n");
 //                printf("valor valido");
-                } else {
-                    printf("Error en el valor ingresado. Rango soportado: [1,9]\n");
-                    continue;
-                }
+                    } else {
+                        printf("Error en el valor ingresado. Rango soportado: [1,9]\n");
+                    }
 
+                } else {
+                    printf("Error en los indices. Rango soportado: [1,9]\n");
+                }
             } else {
-                printf("Error en los indices. Rango soportado: [1,9]\n");
-                continue;
+                printf("Modo no soportado \n");
             }
         }
 
-        printf("Modo no soportado \n");
-
-    } while (strcmp(comando, "exit\n") != 0);
+        fgets(input, 13, stdin);
+    }
 
     free(input);
     free(comando);
